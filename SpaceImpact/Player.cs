c71c7b834697de88a1a0ebc344666cfc913bee17
Cathal Cronin <cathaldcronin1@yahoo.co.uk>
2013-11-09 18:50:39 +0000

    Added player class and shiphull class with move commands

---
+++ SpaceImpact/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+namespace SpaceImpact
+{
+    class Player : ShipHull, Ship
+    {
+        public Player(Texture2D textureImage, Vector2 position, Vector2 speed)
+            : base(textureImage, position, speed)
+        {
+
+        }
+    }
+}
