7ec3f1689be6584cf98f60be7db9f362a7bfbabd
dualtagh <murray.dualtagh@gmail.com>
2013-11-19 19:42:45 +0000

    Added images, Ship State interface

---
+++ SpaceImpact/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceImpact
+{
+    public class Projectile
+    {
+        private Texture2D texture;
+        private Vector2 direction;
+        private int speed;
+        public Vector2 Position { get; private set;}
+
+        public Projectile(Texture2D tex, Vector2 pos, Vector2 dir, int spd)
+        {
+            texture = tex;
+            Position = pos;
+            direction = dir;
+            speed = spd;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            direction.Normalize();
+            Position += direction * speed;
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(texture, Position, Color.White);
+        }
+    }
+}
---
+++ SpaceImpact/State/I_ShipState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SpaceImpact
+{
+    public interface I_ShipState
+    {
+        void Update(GameTime gameTime, ShipHull ship);
+    }
+}
