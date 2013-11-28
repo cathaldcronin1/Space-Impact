879b8730242a9d060189bd8b9aeacdf6c0b9efb0
Cathal Cronin <cathaldcronin1@yahoo.co.uk>
2013-11-28 23:08:34 +0000

    Added player movement unit tests

---
+++ SpaceImpact/UnitTest/Player_Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+using SpaceImpact;
+
+namespace SpaceImpact
+{
+    [TestFixture]
+    class Player_Tests
+    {
+        float oldPlayerPosition;
+        float newPlayerPosition;
+        Player p = new Player(null, Vector2.Zero, Vector2.Zero);
+
+        [Test]
+        public void Player_Move_Up()
+        {
+            oldPlayerPosition = p.Position.Y;
+            p.MoveUp();
+            newPlayerPosition = p.Position.Y;
+
+            Assert.AreNotEqual(newPlayerPosition, oldPlayerPosition);
+        }
+
+        [Test]
+        public void Player_Move_Down()
+        {
+
+            oldPlayerPosition = p.Position.Y;
+            p.MoveDown();
+            newPlayerPosition = p.Position.Y;
+
+            if(newPlayerPosition >= oldPlayerPosition)
+                Assert.AreNotEqual(newPlayerPosition, oldPlayerPosition);
+        }
+
+        [Test]
+        public void Player_Move_Left()
+        {
+            oldPlayerPosition = p.Position.X;
+            p.MoveLeft();
+            newPlayerPosition = p.Position.X;
+
+            if(newPlayerPosition < oldPlayerPosition)
+                Assert.AreNotEqual(newPlayerPosition, oldPlayerPosition);
+        }
+
+        [Test]
+        public void Player_Move_Right()
+        {
+            oldPlayerPosition = p.Position.X;
+            p.MoveRight();
+            newPlayerPosition = p.Position.X;
+
+            if (newPlayerPosition > oldPlayerPosition)
+                Assert.AreNotEqual(newPlayerPosition, oldPlayerPosition);
+        }
+    }
+}
